758f6e53047893cdc25f29e44aa7584a3bfb2460
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2021-02-16 13:33:47 -0500

    add py dice game

---
+++ py-dice-game/dice_game.py
@@ -0,0 +1,25 @@
+# Your task is to create a dice game for three players
+
+def start_game():
+    '''Obtain the three players' names and create
+    a die. Return the players and the die'''
+    players = []
+    die = []
+    return players, die
+
+def play_game():
+    '''Game continues with turns until one player wins'''
+    #call start_game() to obtain the players 
+    and the die
+    players, die = start_game()
+    score = {}
+    # create a turn where players each roll the die
+    # create a rule for who wins the turn
+    # keep track of the score for each player in the score dictionary
+    # determine how someone wins the game
+    # continue executing turns until someone wins
+    # end the game when someone wins and print out the final score
+    
+
+play_game()
+
