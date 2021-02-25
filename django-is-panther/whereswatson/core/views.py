bc9c85a3630a17c1f076a4e6922ce51a9b2b1a5c
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2021-02-24 20:16:18 -0500

    finish tiny app

--- django-is-panther/whereswatson/core/views.py
+++ django-is-panther/whereswatson/core/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
-
+from .models import Answer
 
 # Create your views here.
+
+def get_info_from_pretend_api():
+    return True
+
 def index(request):
-    return  render(request, 'core/index.html')
+    if not get_info_from_pretend_api():
+        answer = Answer.objects.get(text='NO')
+    else: 
+        answer = Answer.objects.get(text='YES')
+    return  render(request, 'core/index.html', {'answer': answer})
