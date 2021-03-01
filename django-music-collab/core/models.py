ae6afaeeaf7e3ed174044d66e601144308134ff3
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2021-03-01 11:14:43 -0500

    add Album model

--- django-music-collab/core/models.py
+++ django-music-collab/core/models.py
@@ -3,3 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=280)
+    artist = models.CharField(max_length=280)
+    release_date = models.DateField()
